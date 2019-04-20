0d2aa3b4785176f6f2f805879e04fca1db532dc7
KisaragiEffective <kisaragi.effective@gmail.com>
2019-04-20 20:13:51 +0900

    [UPDATE] コレクションの型として使われているためequalsとhashCodeを実装 (AUTO GENERATED BY IDEA)

--- src/main/java/com/github/unchama/seichiassist/data/RankData.java
+++ src/main/java/com/github/unchama/seichiassist/data/RankData.java
@@ -1,5 +1,7 @@
 package com.github.unchama.seichiassist.data;
 
+import java.util.Objects;
+
 public class RankData {
 	public String name;
 	public int level;
@@ -9,6 +11,25 @@ public class RankData {
 	public int p_vote;
 	public int premiumeffectpoint;
 	public int p_apple;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		RankData rankData = (RankData) o;
+		return level == rankData.level &&
+				totalbreaknum == rankData.totalbreaknum &&
+				playtick == rankData.playtick &&
+				p_vote == rankData.p_vote &&
+				premiumeffectpoint == rankData.premiumeffectpoint &&
+				p_apple == rankData.p_apple &&
+				Objects.equals(name, rankData.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, level, totalbreaknum, playtick, p_vote, premiumeffectpoint, p_apple);
+	}
 
 	public RankData(){
 		name = null;
