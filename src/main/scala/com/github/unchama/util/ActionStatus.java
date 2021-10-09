39adba78da8c64067f3e263932eb38da71a54abb
KisaragiEffective <kisaragi.effective@gmail.com>
2021-10-09 18:18:44 +0900

    [update] deprecate ActionStatus

--- src/main/scala/com/github/unchama/util/ActionStatus.java
+++ src/main/scala/com/github/unchama/util/ActionStatus.java
@@ -2,7 +2,8 @@ package com.github.unchama.util;
 
 /**
  * @author kory
- * <p>
- * 失敗する可能性のある副作用の成否状態の列挙
+ * <p>失敗する可能性のある副作用の成否状態の列挙</p>
+ * @deprecated please use {@link scala.util.Either}
  */
+@Deprecated
 public enum ActionStatus {Ok, Fail}
